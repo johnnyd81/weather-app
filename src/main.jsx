092da0f1c42defc455450236abf5e368593d8895
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-10-02 20:34:59 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
-//main file that renders the application
+//main entry file that renders the entire application
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
